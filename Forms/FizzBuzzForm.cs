f3b7f174199b2096c59a641ba05fcf90d62081a5
Ateox <126567320+Ateox@users.noreply.github.com>
2023-03-28 01:08:41 +0200

    Add files via upload

---
+++ Forms/FizzBuzzForm.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FizzBuzzWeb.Forms
+{
+    public class FizzBuzzForm
+    {
+        public string Information { get; set; }
+
+        [Display(Name = "Twoje imię: ")]
+        [Required(ErrorMessage = "Pole jest obowiązkowe"), MaxLength(100)]
+        public string Name { get; set; }
+        [Display(Name = "Twój rok urodzenia")]
+        [Required(ErrorMessage = "Pole jest obowiązkowe"), Range(1899, 2022, ErrorMessage = "Oczekiwana wartość {0} z zakredu {1} i {2}.")]
+        public int? Year { get; set; }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using FizzBuzzWeb.Forms;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace FizzBuzzWeb.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        [BindProperty]
+        public FizzBuzzForm FizzBuzz { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string Name { get; set; }
+        public string Year { get; set; }
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                if (FizzBuzz.Year % 4 == 0)
+                {
+                    FizzBuzz.Information = "To był rok przestępny.";
+                }
+                else
+                {
+                    FizzBuzz.Information = "To nie był rok przestępny.";
+                }
+                HttpContext.Session.SetString("Data",JsonConvert.SerializeObject(FizzBuzz));
+                //return RedirectToPage("./SavedInSession");
+            }
+            
+            return Page();
+        }
+    }
+}
